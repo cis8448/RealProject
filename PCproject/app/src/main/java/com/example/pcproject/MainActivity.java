cc2c45d94ff365f1953d1fdd587cf79023e3e6d5
jungsangjoon <tkdtkd5222@gmail.com>
2019-05-14 08:55:02 +0900

    no message

--- PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
+++ PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
@@ -14,15 +14,13 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 
