bae546bebaf0ac3faafcaffc3b9445d24305331c
PcProject <cis8448@naver.com>
2019-05-13 20:14:33 +0900

    no message

--- PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
+++ PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pcproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements {
 
 
     @Override
