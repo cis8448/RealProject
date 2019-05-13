76765a7cfda65ec7aff56c90988417a9339c4570
Hanjooooo <lok1532@naver.com>
2019-05-14 08:57:05 +0900

    no message

--- PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
+++ PCproject/app/src/main/java/com/example/pcproject/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements {
+public class MainActivity extends AppCompatActivity{
 
 
     @Override
